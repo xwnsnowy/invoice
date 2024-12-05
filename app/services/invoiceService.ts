fc314734661a315f6f8e54e8446856478762e68c
Nguyen Tien Thanh <thanhnthe163528@fpt.edu.vn>
2024-12-05 14:30:08 +0700

    feat: implement and display invoice list

---
+++ app/services/invoiceService.ts
@@ -0,0 +1,23 @@
+import prisma from "@/app/utils/db";
+
+export async function getUserInvoices(userId: string) {
+  const data = await prisma.invoice.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      id: true,
+      clientName: true,
+      total: true,
+      createdAt: true,
+      status: true,
+      invoiceNumber: true,
+      currency: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+}
--- components/InvoiceList.tsx
+++ components/InvoiceList.tsx
@@ -1,4 +1,9 @@
+import { getUserInvoices } from "@/app/services/invoiceService";
+import prisma from "@/app/utils/db";
+import { formatCurrency } from "@/app/utils/formatCurrenct";
+import { requireAuth } from "@/app/utils/hooks";
 import { InvoiceActions } from "@/components/InvoiceActions";
+import { Badge } from "@/components/ui/badge";
 import {
   Table,
   TableBody,
@@ -8,30 +13,47 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-export function InvoiceList() {
+export async function InvoiceList() {
+  const session = await requireAuth();
+  const data = await getUserInvoices(session.user?.id as string);
   return (
     <Table>
       <TableHeader>
         <TableRow>
           <TableHead className="">Invoice ID</TableHead>
-          <TableHead className="">Status</TableHead>
-          <TableHead className="">Method</TableHead>
+          <TableHead className="">Customer</TableHead>
           <TableHead className="">Amount</TableHead>
+          <TableHead className="">Status</TableHead>
           <TableHead className="">Date</TableHead>
           <TableHead className="text-right">Action</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        <TableRow>
-          <TableCell className="font-medium">INV-0001</TableCell>
-          <TableCell className="font-medium">Paid</TableCell>
-          <TableCell className="font-medium">Cash</TableCell>
-          <TableCell className="font-medium">Rs. 1000</TableCell>
-          <TableCell className="font-medium">2023-01-01</TableCell>
-          <TableCell className="text-right">
-            <InvoiceActions />
-          </TableCell>
-        </TableRow>
+        {data.map((invoice) => (
+          <TableRow key={invoice.id}>
+            <TableCell className="font-medium">
+              {invoice.invoiceNumber}
+            </TableCell>
+            <TableCell className="font-medium">{invoice.clientName}</TableCell>
+            <TableCell className="font-medium">
+              {formatCurrency({
+                amount: invoice.total,
+                currency: invoice.currency as any,
+              })}
+            </TableCell>
+            <TableCell className="font-medium">
+              <Badge>{invoice.status}</Badge>
+            </TableCell>
+            <TableCell className="font-medium">
+              {new Intl.DateTimeFormat("en-US", {
+                dateStyle: "long",
+              }).format(invoice.createdAt)}
+            </TableCell>
+            <TableCell className="text-right">
+              <InvoiceActions />
+            </TableCell>
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   );
